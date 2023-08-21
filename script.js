b56c06656c28141f8320fd081322bc3c304ab6a8
anacarolinabmv <141560459+anacarolinabmv@users.noreply.github.com>
2023-08-21 12:07:26 +0200

    Form validation for name and card numbrr

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
 'use strict';
+
+const inputCardholder = document.getElementById('cardholder');
+const inputCardNumber = document.getElementById('card--number');
+const inputMonth = document.getElementById('month');
+const inputYear = document.getElementById('year');
+const btnConfirm = document.getElementById('submit');
+
+const validateForm = function () {};
+
+const restoreError = function (element) {
+  const errorLabel = element.parentElement.querySelector('small');
+  errorLabel.classList.remove('display--error');
+};
+
+const renderError = function (element) {
+  const errorLabel = element.parentElement.querySelector('small');
+  errorLabel.classList.add('display--error');
+};
+
+const validateName = function () {
+  const cardName = inputCardholder.value;
+  if (!cardName) {
+    renderError(inputCardholder);
+    return;
+  }
+};
+
+const validateCardNumber = function () {
+  const cardNumber = inputCardNumber.value;
+  if (!cardNumber) {
+    renderError(inputCardNumber);
+    return;
+  }
+
+  const regex = /^[0-9\s]*$/;
+  const test = regex.test(cardNumber);
+
+  if (!test) renderError(inputCardNumber);
+};
+
+btnConfirm.addEventListener('click', (event) => {
+  event.preventDefault();
+  validateName();
+  validateCardNumber();
+});
+inputCardholder.addEventListener('focus', function () {
+  restoreError(this);
+});
+inputCardNumber.addEventListener('focus', function () {
+  restoreError(this);
+});
